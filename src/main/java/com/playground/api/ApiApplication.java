5377ff7f7982a06056d00699db5dbb09922e7100
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-05-24 13:24:57 -0400

    chore(api): Update base package scanning in ApiApplication

--- src/main/java/com/playground/api/ApiApplication.java
+++ src/main/java/com/playground/api/ApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
-		scanBasePackages = {"com.playground.api", "com.playground.utils"}
+		scanBasePackages = {"com.playground.api"}
 )
 public class ApiApplication {
 

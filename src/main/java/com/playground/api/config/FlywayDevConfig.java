9c4b6883e998bd0f763d4a4d5d23fe179ba00fdd
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-04 13:20:51 -0400

    feat(database): Integrate Flyway for database migrations and seeding

---
+++ src/main/java/com/playground/api/config/FlywayDevConfig.java
@@ -0,0 +1,18 @@
+package com.playground.api.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class FlywayDevConfig {
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> {
+            flyway.clean();
+            flyway.migrate();
+        };
+    }
+}

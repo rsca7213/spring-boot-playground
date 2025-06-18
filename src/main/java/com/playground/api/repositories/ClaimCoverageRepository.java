dca168d66b01b42c427c5d8bb5bd142460ac9b37
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-18 18:49:24 -0400

    feat(claims): Add ClaimCoverageRepository and countByPolicyId method in ClaimRepository

--- src/main/java/com/playground/api/repositories/ClaimCoverageRepository.java
+++ src/main/java/com/playground/api/repositories/ClaimCoverageRepository.java
@@ -1,10 +1,9 @@
 package com.playground.api.repositories;
 
-import com.playground.api.entities.Claim;
+import com.playground.api.entities.ClaimCoverage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClaimRepository extends JpaRepository<Claim, Integer> {
-    Integer countByPolicyId(Integer policyId);
+public interface ClaimCoverageRepository extends JpaRepository<ClaimCoverage, Integer> {
 }

0fc9dc022958d768efca3bbe79461c4b134f89de
Ricardo Salvatorelli <rsca4321@gmail.com>
2025-06-25 20:31:18 -0400

    refactor(claims): Remove unused Min validation import from CreateClaimQuery

--- src/main/java/com/playground/api/dtos/claims/CreateClaimQuery.java
+++ src/main/java/com/playground/api/dtos/claims/CreateClaimQuery.java
@@ -2,7 +2,6 @@ package com.playground.api.dtos.claims;
 
 import com.playground.api.enums.CreateClaimProcess;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Min;
 import lombok.Builder;
 import lombok.Value;
 
